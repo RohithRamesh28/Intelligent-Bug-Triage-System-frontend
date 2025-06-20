3b8af7f106a97983b8e0e06028d727f32e528555
RohithRamesh28 <rurohith28@gmail.com>
2025-06-20 17:33:48 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Important for correct route resolution
-  build: {
-    outDir: 'dist', // Default, but you can change if needed
-  },
-  server: {
-    port: 3000,
-    open: true,
-  }
+  base: '/', // critical for SPA routing
 });
